c992d96fa15dafc1a1b1f417e137224e601255a1
Hugo Cruz <hugomfcruz@gmail.com>
2016-06-02 08:40:03 +0100

    Fixing bug on polynomial root calculation

--- week4/calculator/src/main/scala/calculator/Polynomial.scala
+++ week4/calculator/src/main/scala/calculator/Polynomial.scala
@@ -4,15 +4,17 @@ object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
     //Δ = b² - 4ac
-    new Signal(b() * b() - 4 * a() * b())
+    new Signal(Math.pow(b(),2) - 4 * a() * c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
     //(-b ± √Δ) / 2a
+    val delta = computeDelta(a, b, c)()
+    if(delta < 0) Set()
     new Signal(Set(
-      (-b() + Math.sqrt(computeDelta(a, b, c)())) / (2 * a()),
-      (-b() - Math.sqrt(computeDelta(a, b, c)())) / (2 * a())
+      (-b() + Math.sqrt(delta)) / (2 * a()),
+      (-b() - Math.sqrt(delta)) / (2 * a())
     ))
   }
 }
