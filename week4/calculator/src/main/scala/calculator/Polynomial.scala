de79359ceba7b5cd54abe34e7d27970ca7af0acf
Hugo Cruz <hugo.cruz@openbet.com>
2016-06-07 13:35:14 +0100

    Fixing incorrect usage of delta in Polynomial calculations

--- week4/calculator/src/main/scala/calculator/Polynomial.scala
+++ week4/calculator/src/main/scala/calculator/Polynomial.scala
@@ -10,11 +10,11 @@ object Polynomial {
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
     //(-b ± √Δ) / 2a
-    val delta = computeDelta(a, b, c)()
-    if(delta < 0) Set()
-    new Signal(Set(
-      (-b() + Math.sqrt(delta)) / (2 * a()),
-      (-b() - Math.sqrt(delta)) / (2 * a())
+    new Signal(
+      if(delta() < 0) Set()
+      else Set(
+        (-b() + Math.sqrt(delta())) / (2 * a()),
+        (-b() - Math.sqrt(delta())) / (2 * a())
     ))
   }
 }
